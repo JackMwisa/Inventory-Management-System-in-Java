bbfa70f18b37ba9c9d12f08e6bfb3c9e0f5a878b
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-05-11 13:51:41 +0300

    .

--- config/DBConnection.java
+++ config/DBConnection.java
@@ -9,6 +9,15 @@ public class DBConnection {
     private static final String USER = "root";
     private static final String PASSWORD = "jack";
 
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found.");
+            e.printStackTrace();
+        }
+    }
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
