0422a76358390b24825c906607f414a7199a6641
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-05-11 12:51:57 +0300

    Update CartItem.java

--- model/CartItem.java
+++ model/CartItem.java
@@ -1,4 +1,39 @@
 package model;
 
 public class CartItem {
+    private int id;
+    private int userId;
+    private int productId;
+    private int quantity;
+
+    public CartItem(int id, int userId, int productId, int quantity) {
+        this.id = id;
+        this.userId = userId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public CartItem(int userId, int productId, int quantity) {
+        this(0, userId, productId, quantity);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
